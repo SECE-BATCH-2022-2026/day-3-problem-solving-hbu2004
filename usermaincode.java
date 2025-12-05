dd3bd8d57ec9ea131bd163e351201f8eb41e352c
Hari <haribabu082004@gmail.com>
2025-12-05 11:45:54 +0530

    String code

---
+++ usermaincode.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+        int c=usermaincode.countocc(s1,s2);
+        System.out.println(c);
+    }
+}
+
+class usermaincode{
+    public static int countocc(String s1,String s2){
+        int count = 0;
+        int l1 = s1.length();
+        int l2 = s2.length();
+        for (int i = 0; i <= l1 - l2; i++) {
+            int match = 0;
+            for (int j = 0; j < l2; j++) {
+                if (s1.charAt(i + j) == s2.charAt(j)) {
+                    match++;
+                } 
+            }
+            if (match == l2) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+
+
